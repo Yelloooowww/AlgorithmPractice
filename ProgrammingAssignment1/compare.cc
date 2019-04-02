f731e430d5c0994ee2c02a3d867e9e0f47c8da8c
Yellow <cococing0324@gmail.com>
2019-04-02 13:19:30 +0800

    改用動態空間

--- ProgrammingAssignment1/compare.cc
+++ ProgrammingAssignment1/compare.cc
@@ -20,31 +20,34 @@ void sub(int *A,int *B,int *C,int size){
 
 
 void multiply(int *A,int *B,int *C,int n){
+  //mod 101
+  for(int i=0;i<n*n;i++){
+    *(A+i)=*(A+i) %101;
+    *(B+i)=*(B+i) %101;
+  }
+
+
   if(n<=1){
     *C = (*A) * (*B);
     return;
   }else if(n>1){
-    int M1[n*n/4];
-    int M2[n*n/4];
-    int M3[n*n/4];
-    int M4[n*n/4];
-    int M5[n*n/4];
-    int M6[n*n/4];
-    int M7[n*n/4];
-    int C11[n*n/4];
-    int C12[n*n/4];
-    int C21[n*n/4];
-    int C22[n*n/4];
-    int A11[n*n/4];
-    int A12[n*n/4];
-    int A21[n*n/4];
-    int A22[n*n/4];
-    int B11[n*n/4];
-    int B12[n*n/4];
-    int B21[n*n/4];
-    int B22[n*n/4];
-    int tmp1[n*n/4];
-    int tmp2[n*n/4];
+    int *ptr_M1=new int[n*n/4];
+    int *ptr_M2=new int[n*n/4];
+    int *ptr_M3=new int[n*n/4];
+    int *ptr_M4=new int[n*n/4];
+    int *ptr_M5=new int[n*n/4];
+    int *ptr_M6=new int[n*n/4];
+    int *ptr_M7=new int[n*n/4];
+    int *ptr_A11=new int[n*n/4];
+    int *ptr_A12=new int[n*n/4];
+    int *ptr_A21=new int[n*n/4];
+    int *ptr_A22=new int[n*n/4];
+    int *ptr_B11=new int[n*n/4];
+    int *ptr_B12=new int[n*n/4];
+    int *ptr_B21=new int[n*n/4];
+    int *ptr_B22=new int[n*n/4];
+    int *ptr_tmp1=new int[n*n/4];
+    int *ptr_tmp2=new int[n*n/4];
 
 
     //divide A to A11 A12 A21 A22 & divide B to B11 B12 B21 B22
@@ -58,14 +61,14 @@ void multiply(int *A,int *B,int *C,int n){
       int* temp_address_B21=B+(1*(n*n/2)+0*(n/2))+i*n;
       int* temp_address_B22=B+(1*(n*n/2)+1*(n/2))+i*n;
       for(int j=0;j<n/2;j++){
-        A11[i*(n/2)+j]= *(temp_address_A11+j);
-        A12[i*(n/2)+j]= *(temp_address_A12+j);
-        A21[i*(n/2)+j]= *(temp_address_A21+j);
-        A22[i*(n/2)+j]= *(temp_address_A22+j);
-        B11[i*(n/2)+j]= *(temp_address_B11+j);
-        B12[i*(n/2)+j]= *(temp_address_B12+j);
-        B21[i*(n/2)+j]= *(temp_address_B21+j);
-        B22[i*(n/2)+j]= *(temp_address_B22+j);
+        *(ptr_A11+i*(n/2)+j)= *(temp_address_A11+j);
+        *(ptr_A12+i*(n/2)+j)= *(temp_address_A12+j);
+        *(ptr_A21+i*(n/2)+j)= *(temp_address_A21+j);
+        *(ptr_A22+i*(n/2)+j)= *(temp_address_A22+j);
+        *(ptr_B11+i*(n/2)+j)= *(temp_address_B11+j);
+        *(ptr_B12+i*(n/2)+j)= *(temp_address_B12+j);
+        *(ptr_B21+i*(n/2)+j)= *(temp_address_B21+j);
+        *(ptr_B22+i*(n/2)+j)= *(temp_address_B22+j);
       }
     }
 
@@ -73,44 +76,67 @@ void multiply(int *A,int *B,int *C,int n){
 
     //make M1~M7
     // cout<<"  M1=(A11+A22)(B11+B22)"<<endl;
-    add(A11,A22,tmp1,n*n/4);
-    add(B11,B22,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M1,n/2);
+    add(ptr_A11,ptr_A22,ptr_tmp1,n*n/4);
+    add(ptr_B11,ptr_B22,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M1,n/2);
     // cout<<"  M2=(A21+A22)B11"<<endl;
-    add(A21,A22,tmp1,n*n/4);
-    multiply(tmp1,B11,M2,n/2);
+    add(ptr_A21,ptr_A22,ptr_tmp1,n*n/4);
+    multiply(ptr_tmp1,ptr_B11,ptr_M2,n/2);
     // cout<<"  M3=A11(B12-B22)"<<endl;
-    sub(B12,B22,tmp1,n*n/4);
-    multiply(A11,tmp1,M3,n/2);
+    sub(ptr_B12,ptr_B22,ptr_tmp1,n*n/4);
+    multiply(ptr_A11,ptr_tmp1,ptr_M3,n/2);
     // cout<<"  M4=A22(B21-B11)"<<endl;
-    sub(B21,B11,tmp1,n*n/4);
-    multiply(A22,tmp1,M4,n/2);
+    sub(ptr_B21,ptr_B11,ptr_tmp1,n*n/4);
+    multiply(ptr_A22,ptr_tmp1,ptr_M4,n/2);
     // cout<<"  M5=(A11+A12)B22"<<endl;
-    add(A11,A12,tmp1,n*n/4);
-    multiply(tmp1,B22,M5,n/2);
+    add(ptr_A11,ptr_A12,ptr_tmp1,n*n/4);
+    multiply(ptr_tmp1,ptr_B22,ptr_M5,n/2);
     // cout<<"  M6=(A21-A11)(B11+B12)"<<endl;
-    sub(A21,A11,tmp1,n*n/4);
-    add(B11,B12,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M6,n/2);
+    sub(ptr_A21,ptr_A11,ptr_tmp1,n*n/4);
+    add(ptr_B11,ptr_B12,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M6,n/2);
     // cout<<"  M7=(A12-A22)(B21+B22)"<<endl;
-    sub(A12,A22,tmp1,n*n/4);
-    add(B21,B22,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M7,n/2);
+    sub(ptr_A12,ptr_A22,ptr_tmp1,n*n/4);
+    add(ptr_B21,ptr_B22,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M7,n/2);
+
+    delete [] ptr_A11;
+    delete [] ptr_A12;
+    delete [] ptr_A21;
+    delete [] ptr_A22;
+    delete [] ptr_B11;
+    delete [] ptr_B12;
+    delete [] ptr_B21;
+    delete [] ptr_B22;
+
+    int *ptr_C11=new int[n*n/4];
+    int *ptr_C12=new int[n*n/4];
+    int *ptr_C21=new int[n*n/4];
+    int *ptr_C22=new int[n*n/4];
 
     //make C11 C12 C21 C22
     // cout<<"   C11=M1+M4-M5+M7"<<endl;
-    add(M1,M4,tmp1,n*n/4);
-    sub(tmp1,M5,tmp2,n*n/4);
-    add(tmp2,M7,C11,n*n/4);
+    add(ptr_M1,ptr_M4,ptr_tmp1,n*n/4);
+    sub(ptr_tmp1,ptr_M5,ptr_tmp2,n*n/4);
+    add(ptr_tmp2,ptr_M7,ptr_C11,n*n/4);
     // cout<<"   C12=M3+M5"<<endl;
-    add(M3,M5,C12,n*n/4);
+    add(ptr_M3,ptr_M5,ptr_C12,n*n/4);
     // cout<<"   C21=M2+M4"<<endl;
-    add(M2,M4,C21,n*n/4);
+    add(ptr_M2,ptr_M4,ptr_C21,n*n/4);
     // cout<<"   C22=M1-M2+M3+M6"<<endl;
-    sub(M1,M2,tmp1,n*n/4);
-    add(tmp1,M3,tmp2,n*n/4);
-    add(tmp2,M6,C22,n*n/4);
+    sub(ptr_M1,ptr_M2,ptr_tmp1,n*n/4);
+    add(ptr_tmp1,ptr_M3,ptr_tmp2,n*n/4);
+    add(ptr_tmp2,ptr_M6,ptr_C22,n*n/4);
 
+    delete [] ptr_M1;
+    delete [] ptr_M2;
+    delete [] ptr_M3;
+    delete [] ptr_M4;
+    delete [] ptr_M5;
+    delete [] ptr_M6;
+    delete [] ptr_M7;
+    delete [] ptr_tmp1;
+    delete [] ptr_tmp2;
 
     //put C11~C22 into C
     for(int i=0;i<n/2;i++){
@@ -119,57 +145,91 @@ void multiply(int *A,int *B,int *C,int n){
       int* temp_address_C21=C+(1*(n*n/2)+0*(n/2))+i*n;
       int* temp_address_C22=C+(1*(n*n/2)+1*(n/2))+i*n;
       for(int j=0;j<n/2;j++){
-        *(temp_address_C11+j)=C11[i*(n/2)+j];
-        *(temp_address_C12+j)=C12[i*(n/2)+j];
-        *(temp_address_C21+j)=C21[i*(n/2)+j];
-        *(temp_address_C22+j)=C22[i*(n/2)+j];
+        *(temp_address_C11+j)=  *(ptr_C11+i*(n/2)+j);
+        *(temp_address_C12+j)=  *(ptr_C12+i*(n/2)+j);
+        *(temp_address_C21+j)=  *(ptr_C21+i*(n/2)+j);
+        *(temp_address_C22+j)=  *(ptr_C22+i*(n/2)+j);
       }
     }
+
+
+    delete [] ptr_C11;
+    delete [] ptr_C12;
+    delete [] ptr_C21;
+    delete [] ptr_C22;
+
   }
 }
 
 void Strassen(int *A,int *B,int *C,int M,int N,int K){
-  static int Z=1;//2^0
+  int Z=1;//2^0
   while (Z<max(max(M,N),K)) {
     Z*=2;
   }
 
-  int AA[Z][Z],BB[Z][Z],CC[Z][Z];
+  int *AA=new int[Z*Z];
+  int *BB=new int[Z*Z];
+  int *CC=new int[Z*Z];
 
   //fill A & B with 0
   for(int i=0;i<Z;i++){
     for(int j=0;j<Z;j++){
       if(i<N && j<M){
-        AA[i][j]=*(A+M*i+j);
+        *(AA+i*Z+j)=*(A+M*i+j);
       }else{
-        AA[i][j]=0;
+        *(AA+i*Z+j)=0;
       }
       if(i<M && j<K){
-        BB[i][j]=*(B+K*i+j);
+        *(BB+i*Z+j)=*(B+K*i+j);
       }else{
-        BB[i][j]=0;
+        *(BB+i*Z+j)=0;
       }
     }
   }
 
-  multiply(&AA[0][0],&BB[0][0],&CC[0][0],Z);
-
+  multiply(AA,BB,CC,Z);
 
 
   //put ans into C
   for(int i=0;i<N;i++){
     for(int j=0;j<K;j++){
-      *(C+i*K+j)=CC[i][j];
+      *(C+i*K+j)= *(CC+i*N+j);
+    }
+  }
+}
+
+
+void tradition(int *A,int *B,int *C,int n){
+
+  //mod 101
+  for(int i=0;i<n*n;i++){
+    *(A+i)=*(A+i) %101;
+    *(B+i)=*(B+i) %101;
+  }
+
+
+  //clean C
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      *(C+i*n+j)=0;
     }
   }
 
+
+  //calculating~~~
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      for(int k=0;k<n;k++){
+          *(C+n*i+j) += *((A+n*i+k))* (*(B+k*i+j));
+      }
+    }
+  }
 
 }
 
 int main(){
   ofstream outfile;
   outfile.open("test_result.txt");
-  // outfile<<"   n t_Strassen t_tradition"<<endl;
 
 
   //Start testing~~~
@@ -187,25 +247,14 @@ int main(){
     }
 
     int t_Strassen_start=clock();
-    Strassen(&A[0][0],&B[0][0],&C[0][0], n, n, n);
+    multiply(&A[0][0],&B[0][0],&C[0][0], n);
     int t_Strassen_end=clock();
 
-    //clean C
-    for(int i=0;i<n;i++){
-      for(int j=0;j<n;j++){
-        C[i][j]=0;
-      }
-    }
+
 
     //traditional method~~~
     int t_tradition_start=clock();
-    for(int i=0;i<n;i++){
-	    for(int j=0;j<n;j++){
-  			for(int k=0;k<n;k++){
-  			    C[i][j]+=A[i][k]*B[k][j];
-  			}
-  		}
-  	}
+    tradition(&A[0][0],&B[0][0],&C[0][0], n);
     int t_tradition_end=clock();
 
 
--- ProgrammingAssignment1/zero.cc
+++ ProgrammingAssignment1/zero.cc
@@ -16,37 +16,146 @@ void sub(int *A,int *B,int *C,int size){
 }
 
 
+//
+// void multiply(int *A,int *B,int *C,int n){
+//
+//   if(n<=1){
+//     *C = (*A)*(*B);
+//     return;
+//   }else if(n>1){
+//     int M1[n*n/4];
+//     int M2[n*n/4];
+//     int M3[n*n/4];
+//     int M4[n*n/4];
+//     int M5[n*n/4];
+//     int M6[n*n/4];
+//     int M7[n*n/4];
+//     int C11[n*n/4];
+//     int C12[n*n/4];
+//     int C21[n*n/4];
+//     int C22[n*n/4];
+//     int A11[n*n/4];
+//     int A12[n*n/4];
+//     int A21[n*n/4];
+//     int A22[n*n/4];
+//     int B11[n*n/4];
+//     int B12[n*n/4];
+//     int B21[n*n/4];
+//     int B22[n*n/4];
+//     int tmp1[n*n/4];
+//     int tmp2[n*n/4];
+//
+//
+//     //divide A to A11 A12 A21 A22 & divide B to B11 B12 B21 B22 & init tmp1,tmp2
+//     for(int i=0;i<n/2;i++){
+//       int* temp_address_A11=A+(0*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_A12=A+(0*(n*n/2)+1*(n/2))+i*n;
+//       int* temp_address_A21=A+(1*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_A22=A+(1*(n*n/2)+1*(n/2))+i*n;
+//       int* temp_address_B11=B+(0*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_B12=B+(0*(n*n/2)+1*(n/2))+i*n;
+//       int* temp_address_B21=B+(1*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_B22=B+(1*(n*n/2)+1*(n/2))+i*n;
+//       for(int j=0;j<n/2;j++){
+//         A11[i*(n/2)+j]= *(temp_address_A11+j);
+//         A12[i*(n/2)+j]= *(temp_address_A12+j);
+//         A21[i*(n/2)+j]= *(temp_address_A21+j);
+//         A22[i*(n/2)+j]= *(temp_address_A22+j);
+//         B11[i*(n/2)+j]= *(temp_address_B11+j);
+//         B12[i*(n/2)+j]= *(temp_address_B12+j);
+//         B21[i*(n/2)+j]= *(temp_address_B21+j);
+//         B22[i*(n/2)+j]= *(temp_address_B22+j);
+//
+//       }
+//     }
+//
+//
+//
+//     //make M1~M7
+//     // cout<<"  M1=(A11+A22)(B11+B22)"<<endl;
+//     add(A11,A22,tmp1,n*n/4);
+//     add(B11,B22,tmp2,n*n/4);
+//     multiply(tmp1,tmp2,M1,n/2);
+//     // cout<<"  M2=(A21+A22)B11"<<endl;
+//     add(A21,A22,tmp1,n*n/4);
+//     multiply(tmp1,B11,M2,n/2);
+//     // cout<<"  M3=A11(B12-B22)"<<endl;
+//     sub(B12,B22,tmp1,n*n/4);
+//     multiply(A11,tmp1,M3,n/2);
+//     // cout<<"  M4=A22(B21-B11)"<<endl;
+//     sub(B21,B11,tmp1,n*n/4);
+//     multiply(A22,tmp1,M4,n/2);
+//     // cout<<"  M5=(A11+A12)B22"<<endl;
+//     add(A11,A12,tmp1,n*n/4);
+//     multiply(tmp1,B22,M5,n/2);
+//     // cout<<"  M6=(A21-A11)(B11+B12)"<<endl;
+//     sub(A21,A11,tmp1,n*n/4);
+//     add(B11,B12,tmp2,n*n/4);
+//     multiply(tmp1,tmp2,M6,n/2);
+//     // cout<<"  M7=(A12-A22)(B21+B22)"<<endl;
+//     sub(A12,A22,tmp1,n*n/4);
+//     add(B21,B22,tmp2,n*n/4);
+//     multiply(tmp1,tmp2,M7,n/2);
+//
+//     //make C11 C12 C21 C22
+//     // cout<<"   C11=M1+M4-M5+M7"<<endl;
+//     add(M1,M4,tmp1,n*n/4);
+//     sub(tmp1,M5,tmp2,n*n/4);
+//     add(tmp2,M7,C11,n*n/4);
+//     // cout<<"   C12=M3+M5"<<endl;
+//     add(M3,M5,C12,n*n/4);
+//     // cout<<"   C21=M2+M4"<<endl;
+//     add(M2,M4,C21,n*n/4);
+//     // cout<<"   C22=M1-M2+M3+M6"<<endl;
+//     sub(M1,M2,tmp1,n*n/4);
+//     add(tmp1,M3,tmp2,n*n/4);
+//     add(tmp2,M6,C22,n*n/4);
+//
+//
+//     //put C11~C22 into C
+//     for(int i=0;i<n/2;i++){
+//       int* temp_address_C11=C+(0*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_C12=C+(0*(n*n/2)+1*(n/2))+i*n;
+//       int* temp_address_C21=C+(1*(n*n/2)+0*(n/2))+i*n;
+//       int* temp_address_C22=C+(1*(n*n/2)+1*(n/2))+i*n;
+//       for(int j=0;j<n/2;j++){
+//         *(temp_address_C11+j)=C11[i*(n/2)+j];
+//         *(temp_address_C12+j)=C12[i*(n/2)+j];
+//         *(temp_address_C21+j)=C21[i*(n/2)+j];
+//         *(temp_address_C22+j)=C22[i*(n/2)+j];
+//       }
+//     }
+//
+//   }
+//
+// }
+
 
 void multiply(int *A,int *B,int *C,int n){
-
   if(n<=1){
-    *C = (*A)*(*B);
+    *C = (*A) * (*B);
     return;
   }else if(n>1){
-    int M1[n*n/4];
-    int M2[n*n/4];
-    int M3[n*n/4];
-    int M4[n*n/4];
-    int M5[n*n/4];
-    int M6[n*n/4];
-    int M7[n*n/4];
-    int C11[n*n/4];
-    int C12[n*n/4];
-    int C21[n*n/4];
-    int C22[n*n/4];
-    int A11[n*n/4];
-    int A12[n*n/4];
-    int A21[n*n/4];
-    int A22[n*n/4];
-    int B11[n*n/4];
-    int B12[n*n/4];
-    int B21[n*n/4];
-    int B22[n*n/4];
-    int tmp1[n*n/4];
-    int tmp2[n*n/4];
+    int *ptr_M1=new int[n*n/4];
+    int *ptr_M2=new int[n*n/4];
+    int *ptr_M3=new int[n*n/4];
+    int *ptr_M4=new int[n*n/4];
+    int *ptr_M5=new int[n*n/4];
+    int *ptr_M6=new int[n*n/4];
+    int *ptr_M7=new int[n*n/4];
+    int *ptr_A11=new int[n*n/4];
+    int *ptr_A12=new int[n*n/4];
+    int *ptr_A21=new int[n*n/4];
+    int *ptr_A22=new int[n*n/4];
+    int *ptr_B11=new int[n*n/4];
+    int *ptr_B12=new int[n*n/4];
+    int *ptr_B21=new int[n*n/4];
+    int *ptr_B22=new int[n*n/4];
+    int *ptr_tmp1=new int[n*n/4];
+    int *ptr_tmp2=new int[n*n/4];
 
 
-    //divide A to A11 A12 A21 A22 & divide B to B11 B12 B21 B22 & init tmp1,tmp2
+    //divide A to A11 A12 A21 A22 & divide B to B11 B12 B21 B22
     for(int i=0;i<n/2;i++){
       int* temp_address_A11=A+(0*(n*n/2)+0*(n/2))+i*n;
       int* temp_address_A12=A+(0*(n*n/2)+1*(n/2))+i*n;
@@ -57,15 +166,14 @@ void multiply(int *A,int *B,int *C,int n){
       int* temp_address_B21=B+(1*(n*n/2)+0*(n/2))+i*n;
       int* temp_address_B22=B+(1*(n*n/2)+1*(n/2))+i*n;
       for(int j=0;j<n/2;j++){
-        A11[i*(n/2)+j]= *(temp_address_A11+j);
-        A12[i*(n/2)+j]= *(temp_address_A12+j);
-        A21[i*(n/2)+j]= *(temp_address_A21+j);
-        A22[i*(n/2)+j]= *(temp_address_A22+j);
-        B11[i*(n/2)+j]= *(temp_address_B11+j);
-        B12[i*(n/2)+j]= *(temp_address_B12+j);
-        B21[i*(n/2)+j]= *(temp_address_B21+j);
-        B22[i*(n/2)+j]= *(temp_address_B22+j);
-
+        *(ptr_A11+i*(n/2)+j)= *(temp_address_A11+j);
+        *(ptr_A12+i*(n/2)+j)= *(temp_address_A12+j);
+        *(ptr_A21+i*(n/2)+j)= *(temp_address_A21+j);
+        *(ptr_A22+i*(n/2)+j)= *(temp_address_A22+j);
+        *(ptr_B11+i*(n/2)+j)= *(temp_address_B11+j);
+        *(ptr_B12+i*(n/2)+j)= *(temp_address_B12+j);
+        *(ptr_B21+i*(n/2)+j)= *(temp_address_B21+j);
+        *(ptr_B22+i*(n/2)+j)= *(temp_address_B22+j);
       }
     }
 
@@ -73,44 +181,67 @@ void multiply(int *A,int *B,int *C,int n){
 
     //make M1~M7
     // cout<<"  M1=(A11+A22)(B11+B22)"<<endl;
-    add(A11,A22,tmp1,n*n/4);
-    add(B11,B22,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M1,n/2);
+    add(ptr_A11,ptr_A22,ptr_tmp1,n*n/4);
+    add(ptr_B11,ptr_B22,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M1,n/2);
     // cout<<"  M2=(A21+A22)B11"<<endl;
-    add(A21,A22,tmp1,n*n/4);
-    multiply(tmp1,B11,M2,n/2);
+    add(ptr_A21,ptr_A22,ptr_tmp1,n*n/4);
+    multiply(ptr_tmp1,ptr_B11,ptr_M2,n/2);
     // cout<<"  M3=A11(B12-B22)"<<endl;
-    sub(B12,B22,tmp1,n*n/4);
-    multiply(A11,tmp1,M3,n/2);
+    sub(ptr_B12,ptr_B22,ptr_tmp1,n*n/4);
+    multiply(ptr_A11,ptr_tmp1,ptr_M3,n/2);
     // cout<<"  M4=A22(B21-B11)"<<endl;
-    sub(B21,B11,tmp1,n*n/4);
-    multiply(A22,tmp1,M4,n/2);
+    sub(ptr_B21,ptr_B11,ptr_tmp1,n*n/4);
+    multiply(ptr_A22,ptr_tmp1,ptr_M4,n/2);
     // cout<<"  M5=(A11+A12)B22"<<endl;
-    add(A11,A12,tmp1,n*n/4);
-    multiply(tmp1,B22,M5,n/2);
+    add(ptr_A11,ptr_A12,ptr_tmp1,n*n/4);
+    multiply(ptr_tmp1,ptr_B22,ptr_M5,n/2);
     // cout<<"  M6=(A21-A11)(B11+B12)"<<endl;
-    sub(A21,A11,tmp1,n*n/4);
-    add(B11,B12,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M6,n/2);
+    sub(ptr_A21,ptr_A11,ptr_tmp1,n*n/4);
+    add(ptr_B11,ptr_B12,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M6,n/2);
     // cout<<"  M7=(A12-A22)(B21+B22)"<<endl;
-    sub(A12,A22,tmp1,n*n/4);
-    add(B21,B22,tmp2,n*n/4);
-    multiply(tmp1,tmp2,M7,n/2);
+    sub(ptr_A12,ptr_A22,ptr_tmp1,n*n/4);
+    add(ptr_B21,ptr_B22,ptr_tmp2,n*n/4);
+    multiply(ptr_tmp1,ptr_tmp2,ptr_M7,n/2);
+
+    delete [] ptr_A11;
+    delete [] ptr_A12;
+    delete [] ptr_A21;
+    delete [] ptr_A22;
+    delete [] ptr_B11;
+    delete [] ptr_B12;
+    delete [] ptr_B21;
+    delete [] ptr_B22;
+
+    int *ptr_C11=new int[n*n/4];
+    int *ptr_C12=new int[n*n/4];
+    int *ptr_C21=new int[n*n/4];
+    int *ptr_C22=new int[n*n/4];
 
     //make C11 C12 C21 C22
     // cout<<"   C11=M1+M4-M5+M7"<<endl;
-    add(M1,M4,tmp1,n*n/4);
-    sub(tmp1,M5,tmp2,n*n/4);
-    add(tmp2,M7,C11,n*n/4);
+    add(ptr_M1,ptr_M4,ptr_tmp1,n*n/4);
+    sub(ptr_tmp1,ptr_M5,ptr_tmp2,n*n/4);
+    add(ptr_tmp2,ptr_M7,ptr_C11,n*n/4);
     // cout<<"   C12=M3+M5"<<endl;
-    add(M3,M5,C12,n*n/4);
+    add(ptr_M3,ptr_M5,ptr_C12,n*n/4);
     // cout<<"   C21=M2+M4"<<endl;
-    add(M2,M4,C21,n*n/4);
+    add(ptr_M2,ptr_M4,ptr_C21,n*n/4);
     // cout<<"   C22=M1-M2+M3+M6"<<endl;
-    sub(M1,M2,tmp1,n*n/4);
-    add(tmp1,M3,tmp2,n*n/4);
-    add(tmp2,M6,C22,n*n/4);
+    sub(ptr_M1,ptr_M2,ptr_tmp1,n*n/4);
+    add(ptr_tmp1,ptr_M3,ptr_tmp2,n*n/4);
+    add(ptr_tmp2,ptr_M6,ptr_C22,n*n/4);
 
+    delete [] ptr_M1;
+    delete [] ptr_M2;
+    delete [] ptr_M3;
+    delete [] ptr_M4;
+    delete [] ptr_M5;
+    delete [] ptr_M6;
+    delete [] ptr_M7;
+    delete [] ptr_tmp1;
+    delete [] ptr_tmp2;
 
     //put C11~C22 into C
     for(int i=0;i<n/2;i++){
@@ -119,16 +250,24 @@ void multiply(int *A,int *B,int *C,int n){
       int* temp_address_C21=C+(1*(n*n/2)+0*(n/2))+i*n;
       int* temp_address_C22=C+(1*(n*n/2)+1*(n/2))+i*n;
       for(int j=0;j<n/2;j++){
-        *(temp_address_C11+j)=C11[i*(n/2)+j];
-        *(temp_address_C12+j)=C12[i*(n/2)+j];
-        *(temp_address_C21+j)=C21[i*(n/2)+j];
-        *(temp_address_C22+j)=C22[i*(n/2)+j];
+        *(temp_address_C11+j)=  *(ptr_C11+i*(n/2)+j);
+        *(temp_address_C12+j)=  *(ptr_C12+i*(n/2)+j);
+        *(temp_address_C21+j)=  *(ptr_C21+i*(n/2)+j);
+        *(temp_address_C22+j)=  *(ptr_C22+i*(n/2)+j);
       }
     }
 
-  }
 
+    delete [] ptr_C11;
+    delete [] ptr_C12;
+    delete [] ptr_C21;
+    delete [] ptr_C22;
+
+  }
 }
+
+
+
 
 void Strassen(int *A,int *B,int *C,int M,int N,int K){
   int Z=1;//2^0
